b3c9798d7a7dbd5b1985f43e91205f6eae948a90
fiqriachmada <achmada1998@gmail.com>
2021-03-17 17:10:21 +0700

    v2-frt

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,83 +1,100 @@
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 module.exports = {
-  title: 'SMARTONE',
-  tagline: 'The tagline of my site',
-  url: 'https://your-docusaurus-test-site.com',
-  baseUrl: '/',
-  onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
-  favicon: 'img/favicon.svg',
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  title: "SMARTONE",
+  tagline: "The tagline of my site",
+  url: "https://your-docusaurus-test-site.com",
+  baseUrl: "/",
+  onBrokenLinks: "throw",
+  onBrokenMarkdownLinks: "warn",
+  favicon: "img/favicon.svg",
+  organizationName: "facebook", // Usually your GitHub org/user name.
+  projectName: "docusaurus", // Usually your repo name.
   themeConfig: {
     navbar: {
+      style: "dark",
       // title: 'My Site',
       logo: {
-        alt: 'My Site Logo',
-        src: 'img/logo.svg',
+        alt: "My Site Logo",
+        src: "img/logo.svg",
       },
       items: [
         {
-          to: 'docs/',
-          activeBasePath: 'docs',
-          label: 'Docs',
-          position: 'right',
+          type: 'localeDropdown',
+
+          //// Optional
+          position: 'left',
+          // Add additional dropdown items at the beginning/end of the dropdown.
+          dropdownItemsBefore: [],
+          dropdownItemsAfter: [
+            {
+              to: 'https://my-site.com/help-us-translate',
+              label: 'Help us translate',
+            },
+          ],
         },
-        {to: 'blog', label: 'Blog', position: 'right'},
+      ],
+      items: [
         {
-          href: 'https://github.com/facebook/docusaurus',
-          label: 'GitHub',
-          position: 'right',
+          to: "docs/",
+          activeBasePath: "docs",
+          label: "Docs",
+          position: "right",
+        },
+        { to: "blog", label: "Blog", position: "right" },
+        {
+          href: "https://github.com/fiqriachmada/smart",
+          label: "GitHub",
+          position: "right",
         },
       ],
     },
     footer: {
-      style: 'dark',
+      style: "dark",
       links: [
         {
-          title: 'Find we at',
+          title: "Find we at",
           items: [
             {
-              label: 'Facebook',
-              to: 'Facebook/',
+              label: "Facebook",
+              to: "Facebook/",
             },
             {
-              label: 'Instagram',
-              to: 'Instagram/',
+              label: "Instagram",
+              to: "Instagram/",
             },
             {
-              label: 'Twitter',
-              to: 'Twitter/',
+              label: "Twitter",
+              to: "Twitter/",
             },
             {
-              label: 'LinkedIn',
-              to: 'LinkedIn/',
+              label: "LinkedIn",
+              to: "LinkedIn/",
             },
           ],
         },
         {
-          title: 'Our',
+          title: "Our",
           items: [
             {
-              label: 'Team',
-              to: 'Team/',
+              label: "Team",
+              to: "Team/",
             },
             {
-              label: 'Works',
-              to: 'Works/',
+              label: "Works",
+              to: "Works/",
             },
           ],
         },
         {
-          title: 'More',
+          title: "More",
           items: [
             {
-              label: 'Blog',
-              to: 'blog',
+              label: "Blog",
+              to: "blog",
             },
             {
-              label: 'GitHub',
-              href: 'https://github.com/facebook/docusaurus',
+              label: "GitHub",
+              href: "https://github.com/fiqriachmada/smart",
             },
           ],
         },
@@ -87,22 +104,24 @@ module.exports = {
   },
   presets: [
     [
-      '@docusaurus/preset-classic',
+      "@docusaurus/preset-classic",
       {
         docs: {
-          sidebarPath: require.resolve('./sidebars.js'),
+          sidebarPath: require.resolve("./sidebars.js"),
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/',
+            "https://github.com/facebook/docusaurus/edit/master/website/",
         },
         blog: {
           showReadingTime: true,
           // Please change this to your repo.
           editUrl:
-            'https://github.com/facebook/docusaurus/edit/master/website/blog/',
+            "https://github.com/facebook/docusaurus/edit/master/website/blog/",
         },
+        // theme: ["theme-blog"],
+        // plugins: ["plugin-content-blog"],
         theme: {
-          customCss: require.resolve('./src/css/custom.css'),
+          customCss: require.resolve("./src/css/custom.css"),
         },
       },
     ],
